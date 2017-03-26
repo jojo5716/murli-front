c00ef23ad95b9461f83cdece85e34ea0a9c7ba61
Jonathan Rodríguez <jrodriguez.5716@gmail.com>
2017-03-26 22:30:36 +0200

    removing user menu

--- modules/components/Header.js
+++ modules/components/Header.js
@@ -32,37 +32,6 @@ export default class App extends React.Component {
                             <span className="gcon gcon-dots-three-vertical f-s-sm"/>
                         </button>
                     </div>
-                    <div className="collapse navbar-collapse" id="roosa-nav-collapse">
-                        <div className="navbar-right">
-                            <ul className="nav navbar-nav">
-                                <li className="rs-user-nav dropdown">
-                                    <a href="javascript:void(0);" className="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        <span className="circle-notification badge-notification bg-success"/>
-                                        <img src="http://gentolab.com/demo/roosa/latest/images/avatars/01.png" className="rs-nav-avatar img-circle" alt="Avatar"/>
-                                            <span className="visible-xs-inline-block m-l">
-                                                Welcome,
-                                                <strong>USER</strong>
-                                            </span>
-                                    </a>
-                                    <ul className="dropdown-menu lg-dropdown">
-                                        <li className="dropdown-header text-uppercase">Account Settings</li>
-                                        <li className="menu-icon">
-                                            <a href="javascript:void(0);">
-                                                <span className="gcon gcon-lock-open rs-dropdown-icon"/>
-                                                Change Password
-                                            </a>
-                                        </li>
-                                        <li className="menu-icon">
-                                            <a href="javascript:void(0);">
-                                                <span className="gcon gcon-log-out rs-dropdown-icon"/>
-                                                Log Out
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
                 </div>
             </nav>
         );
